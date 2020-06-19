9cfd087c10deb70c961313a0ca5e3cb7f70a3b13
AlimiSandeep <43448059+AlimiSandeep@users.noreply.github.com>
2020-06-19 16:52:58 +0530

    UPdated

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -121,7 +121,7 @@ EMAIL_USE_SSL=False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-
+STATIC_PATH=os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 CRISPY_TEMPLATE_PACK='bootstrap4'
